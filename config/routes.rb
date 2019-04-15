b10c7b357fbf62c96ded4fa20ad1ad834f615dd9
ZofiaLorys <zosia.lorys@gmail.com>
2019-04-15 15:01:51 +0200

    Implemented the students index action and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'students/index'
+  root to: 'students#index'
+  get 'students', to: 'students#index'
   get 'students/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
