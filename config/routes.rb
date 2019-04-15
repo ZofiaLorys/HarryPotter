2d6f35acac985c2f52de0c1d0aa914028c3c92d1
ZofiaLorys <zosia.lorys@gmail.com>
2019-04-15 16:53:39 +0200

    Fully added the static about page.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :students, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get 'about', to: 'pages#about', as: 'about'
+
   root to: 'students#index'
 
 end
